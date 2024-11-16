f36b32ebb10e64357ca58faae4017da1b04dba09
Luiz <luiizperren@hotmail.com>
2024-11-16 13:50:48 +0000

    using laptop somewhere else

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,20 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 
+// PrimeVue
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+import 'primeicons/primeicons.css'     // icons
+
 const app = createApp(App)
 
-app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+})
 
 app.mount('#app')
